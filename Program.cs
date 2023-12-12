db403da23d8c55e58242cbadb41b438f58a02e9a
olimov Amir <olimov.amir@mail.ru>
2023-12-12 07:50:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = 5;
+
+        var a = "text";
+
+        var b = new float[10];
+
+        Console.WriteLine(t.GetType());
+        Console.WriteLine(a.GetType());
+        Console.WriteLine(b.GetType());
+
+
+    }
+}
