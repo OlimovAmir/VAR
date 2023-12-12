33c80c331cc9e6c5f9c4d4f8d6c4a29af652461e
olimov Amir <olimov.amir@mail.ru>
2023-12-12 08:58:34 +0300

    created method Function

--- ClassVar.cs
+++ ClassVar.cs
@@ -8,5 +8,20 @@ namespace VAR
 {
     public class ClassVar
     {
+        public void function()
+        {
+            int[] numbers = { 1, 2, 3, 654, 5, 6, 257, 8, 9 };
+            var result = from i in numbers where i > 15 select i;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item}\t");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Type data result:  {result.GetType()} ");
+        }
     }
 }
+    
--- Program.cs
+++ Program.cs
@@ -14,6 +14,17 @@ class Program
         Console.WriteLine(a.GetType());
         Console.WriteLine(b.GetType());
 
+        var c = new Dictionary<int, string>();
+        var array = new List<int>();
 
+        var person = new { Name = "Amir", Age = 36 };
+
+
+        VAR.ClassVar var =new VAR.ClassVar();
+
+        var.function();
+        Console.WriteLine(var);
+
+        
     }
 }
